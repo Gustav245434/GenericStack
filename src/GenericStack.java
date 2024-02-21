3a46c54dbe63fd2d240522fa30bc50097d380ef1
Gustav245434 <gglatzel@student.tgm.ac.at>
2024-02-21 15:24:49 +0100

    Add Constructor with parameter for a stack

--- src/GenericStack.java
+++ src/GenericStack.java
@@ -9,4 +9,15 @@ public class GenericStack <T> {
         this.stack = new Object[0];
     }
 
+    public GenericStack(Object[] stack) {
+
+        if(stack == null) throw new IllegalArgumentException("Stack parameter cant be null!"); // Change to StackEmptyException
+
+        for (Object object : stack) {
+            if(object == null) throw new NullPointerException("No object in the stack parameter can be null!");
+        }
+        this.stack = stack;
+
+    }
+
 }
