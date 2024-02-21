7265feffb8f86d1e486a1d0d804a94d52838cefb
Gustav245434 <gglatzel@student.tgm.ac.at>
2024-02-21 16:17:38 +0100

    Add Documentation for all classes

--- src/GenericStack.java
+++ src/GenericStack.java
@@ -1,13 +1,24 @@
 import java.util.Arrays;
 
+/**
+ * Generic Stack class for creating a stack with a variable datatype.
+ * @param <T> The datatype that the stack uses.
+ */
 public class GenericStack <T> {
 
     private Object[] stack;
 
+    /**
+     * Creates an empty stack
+     */
     public GenericStack() {
         this.stack = new Object[0];
     }
 
+    /**
+     * Creates a stack that is filled with the elements that are given over the parameter
+     * @param stack The objects the empty stack is filled with
+     */
     public GenericStack(T[] stack) {
 
         if(stack == null) throw new StackEmtpyException();
@@ -19,12 +30,20 @@ public class GenericStack <T> {
 
     }
 
+    /**
+     * Adds an elements to the stack
+     * @param object The elements that is added to the stack
+     */
     public void push(T object) {
         Object[] newStack = Arrays.copyOf(this.stack, this.stack.length + 1);
         newStack[newStack.length - 1] = object;
         this.stack = newStack;
     }
 
+    /**
+     * Returns the last element added to the stack
+     * @return The element that was added at last
+     */
     public T peek() {
 
         if (this.stack.length == 0) {
@@ -34,15 +53,26 @@ public class GenericStack <T> {
         return (T) this.stack[this.stack.length - 1];
     }
 
-    public void pop() {
+    /**
+     * Removes the last added element from the stack
+     * @return The last added element before removal
+     */
+    public T pop() {
 
         if (this.stack.length == 0) {
             throw new StackEmtpyException();
         }
 
+        T object = (T) this.stack[this.stack.length - 1];
+
         this.stack = Arrays.copyOf(this.stack, this.stack.length - 1);
+        return object;
     }
 
+    /**
+     * Gives back a list of all elements in the stack. Seperated by ";"
+     * @return The list of all elements in the stack. Seperated by ";"
+     */
     public String list() {
 
         StringBuilder returnee = new StringBuilder();
--- src/StackEmtpyException.java
+++ src/StackEmtpyException.java
@@ -1,5 +1,12 @@
+/**
+ * StackEmptyException class is there to give back a better exception when the stack in GenericStack class is empty
+ * @author Gustav Glatzel
+ * @version 2024-02-21
+ */
 public class StackEmtpyException extends RuntimeException {
-
+    /**
+     * Creates a new exception with a basic message. Message: "Stack can not be empty!"
+     */
     public StackEmtpyException() {
         super("Stack can not be empty!");
     }
